3839f0a0a2f6aede97bdbfab3f3e532934942440
Milton <ellimilt@amazon.com>
2020-08-01 16:42:43 -0700

    Added Authentication & Password Length requirement

--- Authenticator /playground.rb
+++ Authenticator /playground.rb
@@ -7,9 +7,44 @@ users = [
     { username: "Rod", password: "Password5"}
 ]
 
+
+# Add password strength function 
+# 1. Minimum Password Strength: Done 
+# 2. Require at least one uppercase letter:
+# 3. Require at least one lowercase letter:
+# 4. Require at least one number: 
+# 5. Require at least one nonalphanumeric character: 
+
+
+def password_strength_length password, list_of_users
+    list_of_users.map do |user_record|
+      user_record[:password] = password
+      if user_record[:password].length <=8
+        puts "The password must be 8 characters or more"
+        break
+      else 
+        return true 
+    end
+  end
+  end
+
+# password_strength_length password, users
+
+  
 # Auth Method: Instead of using braek in exeuction flow below, this can be abstracted into it's own method.
 # Instead of using break in execution flow, this can be referenced used to authenticate users for multiple reasons.
-# def auth_user username, password, list_of_users
+def auth_user username, password, list_of_users
+     # Compare the username received as input to user hash. In ruby this can be specified as simple user[:username] 
+     list_of_users.each do |user_record|
+        if user_record[:username] == username && user_record[:password] == password
+            return username
+            break
+        else
+            puts "Credentials incorrect"
+            break
+        end
+    end
+end
 
 puts "Authenticator" 
 25.times { print -"-" }
@@ -26,20 +61,16 @@ while attempts < 4
     username = gets.chomp
     print "Password: "
     password = gets.chomp
-    # Compare the username received as input to user hash. In ruby this can be specified as simple user[:username] 
-    users.each do |user|
-        if user[:username] == username && user[:password] == password
-            puts user
-            break
-        else
-            puts "Credentials incorrect"
-        end
-    end
+    authentication = auth_user(username, password, users)
+    puts authentication
     puts "Press n to quit or any otehr key to continue: "
     input = gets.chomp.downcase 
     break if input == "n"
     attempts += 1 
 end  
+puts "You have exceeded the number of attempts" if attempts == 4 
+
+
 
 
 
